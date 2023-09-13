8a969ffac802bb1e054f85f16ef71f5445d69501
Bea111 <44231126+Bea111@users.noreply.github.com>
2023-09-13 22:52:06 +0100

    update scrapy for title and text of articles

--- gardening/gardening/spiders/gw_spider.py
+++ gardening/gardening/spiders/gw_spider.py
@@ -2,10 +2,13 @@
 from pathlib import Path
 
 import scrapy
+import json
 
 
-class QuotesSpider(scrapy.Spider):
+class GWSpider(scrapy.Spider):
     name = "gw" # gardeners world
+
+ 
 
     def start_requests(self):
         urls = [
@@ -16,11 +19,18 @@ class QuotesSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        article_dict={"title":[], "text:":[]}
+
         page = response.url.split("/")[-2]
         filename = f"gardeners_world-{page}.html"
-        Path(filename).write_bytes(response.body)
         title=response.xpath("//title/text()").get()
-        text=response.css("div.post__content")
+        text=response.xpath('//div/p/descendant-or-self::*/text()').extract()
+        article_dict={"title":[], "text":[]}
+        article_dict["title"].append(title)
+        article_dict["text"].append(text)
+        bytes_dict = json.dumps(article_dict).encode('utf-8')
+
+        Path(filename).write_bytes(bytes_dict)
         self.log(f"Saved file {filename}")
 
 
